09d199350324702b9df97514b1ffe176c2c0e729
Laimonas <pnlaimonas@gmail.com>
2022-02-21 17:23:33 +0200

    flatList changes

--- Screens/Images.js
+++ Screens/Images.js
@@ -1,6 +1,6 @@
 import react, { useState } from "react";
 import { FlatList, Text, TextInput, View, ScrollView, Button } from 'react-native';
-const Home = ({navigation})=>{
+const Images = ({navigation})=>{
 
     const [text,setText]=useState('')
     const [input,setInput]=useState('')
@@ -8,7 +8,7 @@ const Home = ({navigation})=>{
     const data = {
         items:[
             {
-                name:'Jonas',
+                name:'Krica',
                 klase:'11c'
             },
 
@@ -34,65 +34,29 @@ const Home = ({navigation})=>{
                 name:'Benas',
                 klase:'11b'
             },
-            {
-                name:'Jonas',
-                klase:'11c'
-            },
-
-            {
-                name:'Benas',
-                klase:'11b'
-            },
-            {
-                name:'Jonas',
-                klase:'11c'
-            },
-
-            {
-                name:'Benas',
-                klase:'11b'
-            },
-            {
-                name:'Kristina',
-                klase:'11k'
-            }
+           
         ]}
     
     return(
 <View style={{backgroundColor:'white',flex:1,padding:20}}>
-  
-<Button title={'next-Screen'} onPress={()=>{
-             navigation.navigate('images')   
-            }}/>
-  
-    <Text>
-     {input}
-    </Text>
+    <Button title={'back'} onPress={()=>{
+        navigation.pop()
+    }}/>
     <PropsFunkcija data={data} />
 </View>
 
     )
 }
-export default Home
+export default Images
 
 
 const PropsFunkcija = (props)=>{
 
     return(
         <View>
-         
           <FlatList
-          ItemSeparatorComponent={
-            (({ highlighted }) => (
-              <View
-                style={[
-                  highlighted && { marginLeft: 0 }
-                ]}
-              />
-            ))
-          }
              data={props.data.items}
-          renderItem={({item,index}) => <Text key={index} style={{fontSize:25, color:'green'}}>{item.name} - {item.klase}</Text>}
+          renderItem={({item,index}) => <Text key={index} style={{fontSize:25, color:'green'}}>{item.name} - {item.klase} </Text>}
           />
         </View>
     )
--- navigation/LoginNavigator.js
+++ navigation/LoginNavigator.js
@@ -1,13 +1,15 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../Screens/home.js'
+import Images from '../Screens/Images'
 const Stack = createNativeStackNavigator();
 const LoginStack = () => {
   return (
     <NavigationContainer >
         <Stack.Navigator  >
             <Stack.Screen name="home" component={Home} />
+            <Stack.Screen name="images" component={Images} />
         </Stack.Navigator>
     </NavigationContainer >
   )
