2d8cd82f8eee46a136b4f5c72867dc47138a603c
Laimonas <pnlaimonas@gmail.com>
2022-02-28 17:11:52 +0200

    baigtas be propsu papildomu

--- Screens/Images.js
+++ Screens/Images.js
@@ -1,12 +1,52 @@
 
-import { View,Text } from "react-native"
+import { View,Text, StyleSheet, Image, ScrollView } from "react-native"
 const Images = ()=>{
 
     return(
 
         <View>
-            <Text>paveksliuku ekranas</Text>
+            <ScrollView>
+            <DisplayAnImage imgStyle={styles.tinyLogo}  uri={'https://reactnative.dev/img/tiny_logo.png'}/>
+            <DisplayAnImage imgStyle={styles.bigImg} uri={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7x4BJv5EYlnyGDy3kDTUt91HoHuk7d7UooA&usqp=CAU'}/>
+            <DisplayAnImage imgStyle={styles.tinyLogo}  uri={'https://reactnative.dev/img/tiny_logo.png'}/>
+            <DisplayAnImage imgStyle={styles.tinyLogo} uri={'https://reactnative.dev/img/tiny_logo.png'}/>
+            <Text>paveksliuku ekranasssss</Text>
+            </ScrollView>
         </View>
     )
 }
 export default Images
+
+
+const styles = StyleSheet.create({
+    container: {
+      paddingTop: 50,
+    },
+    tinyLogo: {
+      width: 50,
+      height: 50,
+    },
+    bigImg: {
+      width: '100%',
+      height: 350
+    },
+    logo: {
+      width: 66,
+      height: 58,
+    },
+  });
+  
+  const DisplayAnImage = (props) => {
+
+    return (
+      <View style={styles.container}>
+       
+        <Image
+          style={props.imgStyle}
+          source={{
+            uri: props.uri,
+          }}
+        />
+      </View>
+    );
+  }
